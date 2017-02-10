310ee10d31571d320cfffbc84dd1864b47f4b03f
Alejandro Anzola Avila <anzola95avila@gmail.com>
2017-02-10 18:31:12 -0500

    correccion de clases de equivalencia para pruebas

--- src/test/java/edu/eci/pdsw/test/ControllerTest.java
+++ src/test/java/edu/eci/pdsw/test/ControllerTest.java
@@ -24,59 +24,95 @@ import static org.junit.Assert.*;
 public class ControllerTest {
     
     /**
-     * Prueba Clase de Equivalencia: Rota 90 grados
-     * Deberia rotar una figura 90 grados a la derecha con pivote la parte
-     * inferior izquierda del rectangulo que la contiene
+     * Prueba Clase de Equivalencia: Rota linea vertical
+     * Deberia rotar una figura 90 grados a la derecha cuando la linea es vertical
      * @throws java.lang.Exception
      */
     @Test
-    public void testClaseEquivalenciaRota90Grados() throws Exception {
+    public void testClaseEquivalenciaRotaLineaVertical() throws Exception {
         Controller c = new Controller();
         
-        c.setSelectedElementType(ElementType.Line);
+        Point p1 = new Point(3f, 1f);
+        Point p2 = new Point(3f, 5f);
         
-        Point pi1 = new Point(0f, 0f);
-        Point pi2 = new Point(0f, 1f);
-        c.addShape(pi1, pi2);
+        c.addShape(p1, p2);
         
-        int index = c.getShapes().size()-1;
+        p1 = new Point(3f, 1f);
+        p2 = new Point(7f, 1f);
         
-        c.rotateSelectedShape(index);
+        c.rotateSelectedShape(0);
         
-        ArrayList<Shape> l = new ArrayList(c.getShapes());
-        
-        Assert.assertEquals("No se roto correctamente la figura", new Point(1f,0f), l.get(index).getPoint2());
-        Assert.assertEquals("No se roto correctamente la figura", new Point(0f,0f), l.get(index).getPoint1());
+        Assert.assertEquals("Rotacion incorrecta", p1, c.getShapes().get(0).getPoint1());
+        Assert.assertEquals("Rotacion incorrecta", p2, c.getShapes().get(0).getPoint2());
     }
     
     /**
-     * Prueba Clase de Equivalencia: Rota de pivote
-     * Deberia rotar del pivote inferior izquierdo del rectangulo que rodea la figura
+     * Prueba Clase de Equivalencia: Linea Recta Inclinada Con Pendiente Positiva
+     * Deberia rotar una figura 90 grados a la derecha con pendiente positiva
      * @throws java.lang.Exception
      */
     @Test
-    public void testClaseEquivalenciaRotaDePivote() throws Exception{
+    public void testClaseEquivalenciaLineaRectaInclinadaConPendientePositiva() throws Exception {
         Controller c = new Controller();
         
-        c.setSelectedElementType(ElementType.Line);
+        Point p1 = new Point(1f, 7f);
+        Point p2 = new Point(3f, 11f);
         
-        Point pi1 = new Point(1f, 0f);
-        Point pi2 = new Point(0f, 1f);
-        c.addShape(pi1, pi2);
+        c.addShape(p1, p2);
         
-        int index = c.getShapes().size()-1;
+        p1 = new Point(1f, 7f);
+        p2 = new Point(5f, 5f);
         
-        c.rotateSelectedShape(index);
+        c.rotateSelectedShape(0);
         
-        ArrayList<Shape> l = new ArrayList(c.getShapes());
+        Assert.assertEquals("Rotacion incorrecta", p1, c.getShapes().get(0).getPoint1());
+        Assert.assertEquals("Rotacion incorrecta", p2, c.getShapes().get(0).getPoint2());
+    }
+    
+    /**
+     * Prueba Clase de Equivalencia: Linea Recta Inclinada Con Pendiente Negativa
+     * Deberia rotar una figura 90 grados a la derecha con pendiente negativa
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testClaseEquivalenciaLineaRectaInclinadaConPendienteNegativa() throws Exception {
+        Controller c = new Controller();
         
-        // Pueden estar en cualquier orden los elementos pero ser correctos
-        Point[] p = { new Point(1f,0f), new Point(0f,-1f) };
-        int i = 0;
-        Assert.assertTrue("No se roto correctamente la figura", (l.get(index).getPoint2().equals(p[i = 0]) ) 
-                || (l.get(index).getPoint2().equals(p[i = 1])));
-        i = (i+1) % 2;
-        Assert.assertEquals("No se roto correctamente la figura", p[i] , l.get(index).getPoint1());
+        Point p1 = new Point(1f, 8f);
+        Point p2 = new Point(3f, 4f);
+        
+        c.addShape(p1, p2);
+        
+        p1 = new Point(5f, 4f);
+        p2 = new Point(1f, 2f);
+        
+        c.rotateSelectedShape(0);
+        
+        Assert.assertEquals("Rotacion incorrecta", p1, c.getShapes().get(0).getPoint1());
+        Assert.assertEquals("Rotacion incorrecta", p2, c.getShapes().get(0).getPoint2());
+    }
+    
+    /**
+     * Prueba Clase de Equivalencia: Linea Recta Horizontal
+     * Deberia rotar una figura 90 grados a la derecha cuando es una recta horizontal
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testClaseEquivalenciaLineaRectaHorizontal() throws Exception {
+        Controller c = new Controller();
+        
+        Point p1 = new Point(1f, 5f);
+        Point p2 = new Point(6f, 5f);
+        
+        c.addShape(p1, p2);
+        
+        p1 = new Point(1f, 5f);
+        p2 = new Point(1f, 0f);
+        
+        c.rotateSelectedShape(0);
+        
+        Assert.assertEquals("Rotacion incorrecta", p1, c.getShapes().get(0).getPoint1());
+        Assert.assertEquals("Rotacion incorrecta", p2, c.getShapes().get(0).getPoint2());
     }
     
     /**
